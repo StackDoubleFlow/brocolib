48031a574bbdb7ac89093144e5b345823f3b6db4
StackDoubleFlow <ojaslandge@gmail.com>
2022-02-06 14:14:51 -0600

    Start work on stack analysis

--- src/decompiler.rs
+++ src/decompiler.rs
@@ -1,8 +1,8 @@
 use super::MethodInfo;
 use crate::codegen_data::DllData;
-use bad64::{disasm, Instruction, Op, Operand};
-use petgraph::Graph;
+use bad64::{disasm, Instruction, Op, Operand, Imm, Reg};
 use petgraph::dot::{Dot, Config};
+use petgraph::graph::{Graph, NodeIndex};
 use id_arena::Id;
 
 type RawGraph<'a> = Graph<RawNode<'a>, RawEdge>;
@@ -12,23 +12,36 @@ struct StackValue {
     size: u32,
 }
 
-enum Value {
-    Param(usize),
-    General
+enum ValueSource {
+    Node {
+        idx: NodeIndex,
+        define: usize,
+    }
 }
-type ValueId = Id<Value>;
 
 #[derive(Debug)]
 enum RawNode<'a> {
     EntryToken,
-    Op(Op),
+    Param(usize),
+    Op { 
+        op: Op,
+        num_defines: usize,
+    },
     Operand(&'a Operand),
-    Value(ValueId),
 }
+
+// next instruction analysis
+// 1. loop through each input operand
+// 2. check to see which node the value was defined in
+// 3. create link from node found in last step to current node with concat or split if necessary 
+// 4. put new defines in map
 
 #[derive(Debug)]
 enum RawEdge {
-    Value,
+    Value {
+        define: usize,
+        operand: usize,
+    },
     Chain,
 }
 
@@ -39,18 +52,45 @@ pub fn decompile(codegen_data: &DllData, mi: MethodInfo, data: &[u8]) {
     let mut graph = RawGraph::new();
     let entry = graph.add_node(RawNode::EntryToken);
 
+    // let stack = Vec::new();
+
     // let reg_values = Vec<
 
-    // let mut stack_frame_size = 0;
+    let mut stack_frame_size = 0;
     let mut chain = entry;
     for inst in &dis_method.instrs {
         let op = inst.op();
         let operands = inst.operands();
 
-        let node = graph.add_node(RawNode::Op(op));
+        match op {
+            Op::STR => {
+                let addr = match operands[1] {
+                    Operand::MemPreIdx { reg, imm: Imm::Signed(imm) } => { 
+                        if reg == Reg::SP {
+                            stack_frame_size -= imm;
+                            (reg, imm + stack_frame_size)
+                        } else {
+                            (reg, imm)
+                        }
+                    }
+                    Operand::MemOffset { reg, offset: Imm::Signed(imm), .. } => (reg, imm),
+                    _ => unreachable!()
+                };
+                dbg!(operands[1]);
+                if addr.0 == Reg::SP {
+                    println!("Adding to stack space with size 8 and offset {}", addr.1);
+                } else {
+                    unimplemented!()
+                }
+            }
+            _ => unimplemented!()
+        }
+
+        let node = graph.add_node(RawNode::Op { op, num_defines: 0 });
         for operand in operands {
+            
             let operand_node = graph.add_node(RawNode::Operand(operand));
-            graph.add_edge(node, operand_node, RawEdge::Value);
+            graph.add_edge(node, operand_node, RawEdge::Value { define: 0, operand: 0 });
         }
         graph.add_edge(node, chain, RawEdge::Chain);
         chain = node;
@@ -60,7 +100,7 @@ pub fn decompile(codegen_data: &DllData, mi: MethodInfo, data: &[u8]) {
         // println!("{:02x}{:02x}{:02x}{:02x}  {:?}  {}", data[addr + 3], data[addr + 2], data[addr + 1], data[addr], inst.op(), inst);
     }
 
-    println!("{:?}", Dot::with_config(&graph, &[]));
+    // println!("{:?}", Dot::with_config(&graph, &[]));
 }
 
 #[derive(Debug)]
