31e9ffb2a87f57ab30c803921554d1725b69656d
StackDoubleFlow <ojaslandge@gmail.com>
2026-06-17 11:17:03 -0500

    Fix test cfg features

--- tests/unity-6000_0_65f1.rs
+++ tests/unity-6000_0_65f1.rs
@@ -9,14 +9,14 @@ mod elf_tests {
     use super::*;
 
     #[test]
-    #[cfg(feature = "elf_x64")]
+    #[cfg(feature = "x86_64")]
     fn elf_linux_fixture() -> anyhow::Result<()> {
         common::run_checks_helper(VERSION, "linux", "GameAssembly.so")
             .context("running linux elf fixture")
     }
 
     #[test]
-    #[cfg(feature = "elf_aarch64")]
+    #[cfg(feature = "aarch64")]
     fn elf_android_fixture() -> anyhow::Result<()> {
         common::run_checks_helper(VERSION, "android", "libil2cpp.so")
             .context("running android elf fixture")
@@ -30,7 +30,7 @@ mod pe_tests {
     use super::*;
 
     #[test]
-    #[cfg(feature = "pe_x64")]
+    #[cfg(feature = "x86_64")]
     fn pe_windows_fixture() -> anyhow::Result<()> {
         common::run_checks_helper(VERSION, "windows", "GameAssembly.dll")
             .context("running windows pe fixture")
